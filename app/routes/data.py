dc993fba81625c9c3560d99d7f37879ce05d13bf
Adam Ma <adam.zym@gmail.com>
2025-05-07 15:20:48 -0400

    ...

--- app/routes/data.py
+++ app/routes/data.py
@@ -1,8 +1,24 @@
 from fastapi import APIRouter
 from app.schema.data import HandData
+import httpx
 
 router = APIRouter()
 
-@router.post("/")
+@router.get("/")
 async def get_hand_data(link: str) -> HandData:
-    return None
+    game_id = link.split("/")[-1]
+    poker_now_api_url = f"https://www.pokernow.club/api/games/{game_id}/log_v3"
+
+    all_hands = []
+    i = 1
+
+    async with httpx.AsyncClient() as client:
+        while True:
+            response = await client.get(poker_now_api_url, params={"hand_number": i})
+            hand_data = response.json()
+            if hand_data == []:
+                break
+            all_hands.extend(hand_data)
+            i += 1
+
+    return HandData(data=all_hands)
